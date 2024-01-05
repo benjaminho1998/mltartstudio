ebddbcd60c33e85e654af66901442e6ca2abd026
benjaminho <ho.d.benjamin@gmail.com>
2024-01-04 20:43:29 -0800

    - changed modal image width - moved close button back

--- components/SharedModal.tsx
+++ components/SharedModal.tsx
@@ -80,16 +80,8 @@ export default function SharedModal({
                                 initial="enter"
                                 animate="center"
                                 exit="exit"
-                                className="absolute w-screen h-[calc(100vh-100px)]"
+                                className="absolute w-full h-[calc(100vh-100px)]"
                             >
-                                <div className="absolute z-100 w-full -top-10 left-3">
-                                    <button
-                                        onClick={closeModal}
-                                        className="rounded-full bg-black/50 p-2 z-100 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
-                                    >
-                                        <XMarkIcon className="h-5 w-5" />
-                                    </button>
-                                </div>
                                 <Image
                                     src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${currentImage.public_id}.${currentImage.format}`}
                                     fill
@@ -111,51 +103,55 @@ export default function SharedModal({
                 {/* Buttons + bottom nav bar */}
                 <div className="absolute inset-0 mx-auto flex max-w-7xl items-center justify-center">
                     {/* Buttons */}
-                    {loaded && (
-                        <div className="relative aspect-[3/2] max-h-full w-full">
-                            {navigation && (
-                                <>
-                                    {index > 0 && (
-                                        <button
-                                            className="absolute left-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
-                                            style={{
-                                                transform:
-                                                    'translate3d(0, 0, 0)',
-                                            }}
-                                            onClick={() => {
-                                                setLoaded(false)
-                                                changePhotoId(index - 1)
-                                            }}
-                                        >
-                                            <ChevronLeftIcon className="h-6 w-6" />
-                                        </button>
-                                    )}
-                                    {index + 1 < images.length && (
-                                        <button
-                                            className="absolute right-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
-                                            style={{
-                                                transform:
-                                                    'translate3d(0, 0, 0)',
-                                            }}
-                                            onClick={() => {
-                                                setLoaded(false)
-                                                changePhotoId(index + 1)
-                                            }}
-                                        >
-                                            <ChevronRightIcon className="h-6 w-6" />
-                                        </button>
-                                    )}
-                                </>
-                            )}
-                            <div className="absolute top-0 right-0 flex items-center gap-2 p-3 text-white">
-                                {/*{currentImage.tags.map((tag) => (*/}
-                                {/*    <div className="pt-1 pb-1 pl-2 pr-2 bg-gray-100 text-black rounded">*/}
-                                {/*        {tag}*/}
-                                {/*    </div>*/}
-                                {/*))}*/}
-                            </div>
+                    <div className="relative aspect-[3/2] max-h-full w-full">
+                        {navigation && (
+                            <>
+                                {index > 0 && (
+                                    <button
+                                        className="absolute left-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
+                                        style={{
+                                            transform: 'translate3d(0, 0, 0)',
+                                        }}
+                                        onClick={() => {
+                                            setLoaded(false)
+                                            changePhotoId(index - 1)
+                                        }}
+                                    >
+                                        <ChevronLeftIcon className="h-6 w-6" />
+                                    </button>
+                                )}
+                                {index + 1 < images.length && (
+                                    <button
+                                        className="absolute right-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
+                                        style={{
+                                            transform: 'translate3d(0, 0, 0)',
+                                        }}
+                                        onClick={() => {
+                                            setLoaded(false)
+                                            changePhotoId(index + 1)
+                                        }}
+                                    >
+                                        <ChevronRightIcon className="h-6 w-6" />
+                                    </button>
+                                )}
+                            </>
+                        )}
+                        <div className="absolute top-0 right-0 flex items-center gap-2 p-3 text-white">
+                            {/*{currentImage.tags.map((tag) => (*/}
+                            {/*    <div className="pt-1 pb-1 pl-2 pr-2 bg-gray-100 text-black rounded">*/}
+                            {/*        {tag}*/}
+                            {/*    </div>*/}
+                            {/*))}*/}
                         </div>
-                    )}
+                        <div className="absolute top-0 left-0 flex items-center gap-2 p-3 text-white">
+                            <button
+                                onClick={() => closeModal()}
+                                className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+                            >
+                                <XMarkIcon className="h-5 w-5" />
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </MotionConfig>
